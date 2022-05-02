c6bab4c2dc4c0fc8cb44f96424306ea1b01d7fb2
Elsio Sanchez <45974454+elsiosanchez@users.noreply.github.com>
2022-05-02 15:15:34 -0400

    Update start.sh

--- build/start.sh
+++ build/start.sh
@@ -7,4 +7,4 @@ cd /var/www/adempiere-vue/dist/static/js
 find -name 'app.*.js' -exec sed -i "s|https://api.erpya.com|$API_URL|g" {} \;
 
 # Run app
-yarn run build && yarn run start && tail -f /dev/null
+yarn start && tail -f /dev/null
