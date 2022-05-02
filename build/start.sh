b15e04fb7d05ba3deac4cbc5210f6be20cb80fb3
elsiosanchez <elsiosanches@gmail.com>
2022-05-02 10:55:18 -0400

    rename folder with dist app files

--- build/start.sh
+++ build/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # folder with dist app files
-cd /var/www/adempiere-vue/static/js
+cd /var/www/adempiere-vue/dist/static/js
 
 # Set API Proxy connection
 find -name 'app.*.js' -exec sed -i "s|https://api.erpya.com|$API_URL|g" {} \;
